122495176282cc0b9b71c3e4738abe992448197f
Unknown <jamesterrien95@hotmail.fr>
2017-11-15 16:02:46 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Eclipe-v1/repartition-groupe/src/Promo.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Promo {
+	
+	public String nom;
+	
+	public ArrayList<Eleve> listeEleve = new ArrayList();
+	
+	public ArrayList<Projet> listeProjet = new ArrayList();
+	
+	public ArrayList<Eleve> classementEleves = new ArrayList();
+	
+	public ArrayList listeFinal = new ArrayList();
+	
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
